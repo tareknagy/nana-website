5967c06fbb5654e65c19a47041e3dde25a0ee27e
tareknagy <hello@tareknagy.com>
2021-03-17 11:48:04 +1100

    update project size

--- js/main.js
+++ js/main.js
@@ -1,28 +1,31 @@
-$('.container, #title-text').mousemove(function(event){
+$(document).mousemove(function(event){
     // get frame center
-    // var frameHeight = window.innerHeight / 2; // might not need these two
-    // var frameWidth = window.innerWidth * .2 / 2;
     var frameHeight = window.innerHeight;
     var frameWidth = window.innerWidth;
     var mouseVh = event.pageY / frameHeight * 100;
     var mouseVw = event.pageX / frameWidth * 100;
 
-    // move container element (do i need)
-    // $('.container'). attr('style', `offset-position: ${frameWidth - (event.pageX - frameWidth) / 2}px, ${event.pageY / 1.1}px, 0px)`);
+    // // move container -- UPDATE DIV SIZE TO TURN ON / OFF 
+    // let mW = ((((event.pageX)*100)/frameWidth).toFixed(3))-50;
+    // let mH = ((((event.pageY)*100)/frameHeight).toFixed(3))-50;
+    // $(".container").attr('style', `transform: translate3d(${-mW/2}%, ${-mH/2}%, 0px)`); // if i turn this off, i need to update div size
     
+    // // move thumbnails
+    // $('#P001').attr('style', `transform: translate3d(${mouseVw * 2 - 80}%, ${mouseVh * 2}%, 0px)`); // top left
+    // $('#P002').attr('style', `transform: translate3d(${mouseVw * 3}%, ${mouseVh / 5}%, 0px)`); // top left
+    // $('#P003').attr('style', `transform: translate3d(${((100 + mouseVw) * 1.3 - 100)}%, ${(mouseVh * 0.8 + 150)}%, 0px)`); // bottom right corner
+    // $('#P004').attr('style', `transform: translate3d(${-mW/3}%, ${mH/5 + 350}%, 0px)`); // bottom right
+    // $('#P005').attr('style', `transform: translate3d(${(((100 + mouseVw) / 2) - 80)}%, ${mouseVh / 3 }%, 0px)`); // top right
+    // $('#P006').attr('style', `transform: translate3d(${mouseVw / 4 - 80}%, ${((100 + mouseVh) / 1.5 - 45)}%, 0px)`); // bottom left
+
     // move thumbnails
-    $('#P001').attr('style', `transform: translate3d(${mouseVw / 2}vw, ${mouseVh / 2}vh, 0px)`); // top left
-    $('#P002').attr('style', `transform: translate3d(${mouseVw / 2.5 + 5}vw, ${mouseVh / 2.5 - 2}vh, 0px)`); // top left
-    $('#P003').attr('style', `transform: translate3d(${((100 + mouseVw) / 3 - 17)}vw, ${((100 + mouseVh) / 2 - 19)}vh, 0px)`); // bottom right corner
-    $('#P004').attr('style', `transform: translate3d(${((100 + mouseVw) / 4 - 35)}vw, ${((100 + mouseVh) / 3 + 5)}vh, 0px)`); // bottom right
-    $('#P005').attr('style', `transform: translate3d(${(((100 + mouseVw) / 2) - 80)}vw, ${mouseVh / 3 }vh, 0px)`); // top right
-    $('#P006').attr('style', `transform: translate3d(${mouseVw / 4 - 80}vw, ${((100 + mouseVh) / 1.5 - 45)}vh, 0px)`); // bottom left
-    // $('#P001').attr('style', `transform: translate3d(${event.pageX / 2.1 + 100}px, ${event.pageY / 3 + 100}px, 0px)`);  
-    // $('#P002').attr('style', `transform: translate3d(${event.pageX / 3.5 - 140 }px, ${event.pageY / 3.5 + 90}px, 0px)`);
-    // $('#P003').attr('style', `transform: translate3d(${(window.innerWidth - 570) - ((window.innerWidth - event.pageX) / 2.5)}px, ${(window.innerHeight - 80) - ((window.innerHeight - event.pageY) / 2.5)}px, 0px)`);
-    // $('#P004').attr('style', `transform: translate3d(${(window.innerWidth - 590) - ((window.innerWidth - event.pageX) / 3.5)}px, ${(window.innerHeight - 20) - ((window.innerHeight - event.pageY) / 3)}px, 0px)`); // clean for bottom right edge
-    // $('#P005').attr('style', `transform: translate3d(${(window.innerWidth - 750) - ((window.innerWidth - event.pageX) / 2.5)}px, ${event.pageY / 4 + 90}px, 0px)`);
-    // $('#P006').attr('style', `transform: translate3d(${event.pageX / 3.5 - 750 }px, ${(window.innerHeight - 20) - ((window.innerHeight - event.pageY) / 2.2)}px, 0px)`); // clean for bottom right edge
+    $('#P001').attr('style', `transform: translate3d(${mouseVw / 2}%, ${mouseVh / 2}%, 0px)`); // top left
+    $('#P002').attr('style', `transform: translate3d(${mouseVw / 2.5 + 5}%, ${mouseVh / 2.5 - 2}%, 0px)`); // top left
+    $('#P003').attr('style', `transform: translate3d(${((100 + mouseVw) / 3 - 17)}%, ${((100 + mouseVh) / 2 - 19)}%, 0px)`); // bottom right corner
+    $('#P004').attr('style', `transform: translate3d(${((100 + mouseVw) / 4 - 35)}%, ${((100 + mouseVh) / 3 + 5)}%, 0px)`); // bottom right
+    $('#P005').attr('style', `transform: translate3d(${(((100 + mouseVw) / 2) - 80)}%, ${mouseVh / 3 }%, 0px)`); // top right
+    $('#P006').attr('style', `transform: translate3d(${mouseVw / 4 - 80}%, ${((100 + mouseVh) / 1.5 - 45)}%, 0px)`); // bottom left
+
 });
 
 
@@ -51,11 +54,11 @@ $(document).ready(function() {
         midClick: true,
         disableOn: 700,
         removalDelay: 900
-      });
+    });
 
-    $('.contact-link').magnificPopup({
-    type:'inline',
-    midClick: true,
-    removalDelay: 900
+    $('.contact-link').magnificPopup({ // could compress this into about-link
+        type:'inline',
+        midClick: true,
+        removalDelay: 900
     });
 });
